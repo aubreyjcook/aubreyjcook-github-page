46cb2a09108bab8d5efa3971ce205e43fff5cd52
aubreyjcook <aubreyjcook@gmail.com>
2025-04-26 07:57:48 -0500

    minor index update

--- aubreyjcook-gh-page/src/app/page.tsx
+++ aubreyjcook-gh-page/src/app/page.tsx
@@ -4,8 +4,12 @@ import { Button } from 'flowbite-react';
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <header>
+        <h1 className="text-4xl font-bold text-left">Aubrey Cook</h1>
+        <p className="text-lg text-left">Software Engineer</p>
+      </header>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Button>Click this</Button>
+        <p>Main</p>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         
